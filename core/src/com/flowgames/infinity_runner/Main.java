fb0c5a9e344f0e717b3629159c4764456574f8e4
Floris Bonenkamp <flopje@gmail.com>
2015-11-22 16:43:54 +0100

    Basic Model Loading. But fails when trying to get model.

--- core/src/com/flowgames/infinity_runner/Main.java
+++ core/src/com/flowgames/infinity_runner/Main.java
@@ -2,27 +2,28 @@ package com.flowgames.infinity_runner;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
+
 
 public class Main extends ApplicationAdapter {
 	private PerspectiveCamera camera;
-    private Model model;
-    private ModelInstance modelInstance;
     private ModelBatch modelBatch;
     private Environment environment;
     private CameraInputController cameraInputController;
+    private AssetManager assetManager;
+    private Array<ModelInstance> modelInstances = new Array<ModelInstance>();
+
+    private Boolean loading;
 	
 	@Override
 	public void create () {
@@ -31,28 +32,38 @@ public class Main extends ApplicationAdapter {
 
         setCamera();
         setupLights();
-        createBoxModel();
 
         cameraInputController = new CameraInputController(camera);
         Gdx.input.setInputProcessor(cameraInputController);
+
+        assetManager = new AssetManager();
+        assetManager.load("plane_box.g3db", Model.class);
+
+        loading = true;
 	}
 
 	@Override
 	public void render () {
+
+        if (loading && assetManager.update()) {
+            doneLoading();
+        }
+
         cameraInputController.update();
 
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT | GL30.GL_DEPTH_BUFFER_BIT);
 
         modelBatch.begin(camera);
-        modelBatch.render(modelInstance, environment);
+        modelBatch.render(modelInstances, environment);
         modelBatch.end();
 	}
 
     @Override
     public void dispose() {
-        model.dispose();
         modelBatch.dispose();
+        modelInstances.clear();
+        assetManager.dispose();
     }
 
     /**
@@ -72,22 +83,21 @@ public class Main extends ApplicationAdapter {
     }
 
     /**
-     * Create a simple 3d box to be displayed.
-     */
-    private void createBoxModel() {
-        ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(5f, 5f, 5f,
-                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-        modelInstance = new ModelInstance(model);
-    }
-
-    /**
      * Setup some simple directional and ambient lights.
      */
     private void setupLights() {
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+    }
+
+    /**
+     * Called when AssetManager is done loading the models
+     */
+    private void doneLoading() {
+        Model plane = assetManager.get("plane_box.g3db", Model.class);
+        ModelInstance modelInstance = new ModelInstance(plane);
+        modelInstances.add(modelInstance);
+        loading = false;
     }
 
 }
