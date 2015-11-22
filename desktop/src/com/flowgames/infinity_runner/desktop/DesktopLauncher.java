6a30c5c073689dfbc5a4314c2753919005b238b2
Floris Bonenkamp <flopje@gmail.com>
2015-11-22 14:13:17 +0100

    Initial Commit

---
+++ desktop/src/com/flowgames/infinity_runner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.flowgames.infinity_runner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.flowgames.infinity_runner.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
